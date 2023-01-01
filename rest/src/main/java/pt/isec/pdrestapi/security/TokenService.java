42537554a588dd52900b4e198970c4a5bf4b9b35
SmithCalimero <a21280192@isec.pt>
2023-01-01 23:09:21 +0000

    Fix1

--- rest/src/main/java/pt/isec/pdrestapi/security/TokenService.java
+++ rest/src/main/java/pt/isec/pdrestapi/security/TokenService.java
@@ -31,7 +31,7 @@ public class TokenService
         JwtClaimsSet claims = JwtClaimsSet.builder()
             .issuer("self")
             .issuedAt(now)
-            .expiresAt(now.plus(24, ChronoUnit.HOURS))
+            .expiresAt(now.plus(2, ChronoUnit.MINUTES))
             .subject(authentication.getName())
             .claim("scope", scope)
             .build();
